59ae87421843c0591861c4c89665f5d7cc57f3b8
halassonn <gtm24halas@gmail.com>
2020-11-02 09:01:34 +0700

    try again

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const { autoUpdater } = require('electron-updater');
+const { app, BrowserWindow, ipcMain } = require("electron");
+const { autoUpdater } = require("electron-updater");
+const log = require("electron-log");
 
 let mainWindow;
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -11,44 +12,96 @@ function createWindow () {
       nodeIntegration: true,
     },
   });
-  mainWindow.loadFile('index.html');
-  mainWindow.on('closed', function () {
+  mainWindow.loadFile("index.html");
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on("closed", function () {
     mainWindow = null;
   });
-  
-  mainWindow.once('ready-to-show', () => {
-    autoUpdater.checkForUpdatesAndNotify();
-  });
+
+  autoUpdater.checkForUpdates();
+
+  // mainWindow.once('ready-to-show', () => {
+  //   console.log("Check update")
+  //   autoUpdater.checkForUpdatesAndNotify();
+  // });
 }
 
-app.on('ready', () => {
+app.on("ready", () => {
   createWindow();
 });
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', function () {
+app.on("activate", function () {
   if (mainWindow === null) {
     createWindow();
   }
 });
 
-ipcMain.on('app_version', (event) => {
-  event.sender.send('app_version', { version: app.getVersion() });
+ipcMain.on("app_version", (event) => {
+  event.sender.send("app_version", { version: app.getVersion() });
 });
 
-autoUpdater.on('update-available', () => {
-  mainWindow.webContents.send('update_available');
+//-------------------------------------------------------------------
+// Auto updates
+//-------------------------------------------------------------------
+
+// autoUpdater.on('update-available', () => {
+//   console.log("Available update")
+//   mainWindow.webContents.send('update_available');
+// });
+
+// autoUpdater.on('update-downloaded', () => {
+//   console.log("Download update")
+//   mainWindow.webContents.send('update_downloaded');
+// });
+
+// ipcMain.on('restart_app', () => {
+//   autoUpdater.quitAndInstall();
+// });
+
+const sendStatusToWindow = (text) => {
+  log.info(text);
+  if (mainWindow) {
+    mainWindow.webContents.send("message", text);
+  }
+};
+
+autoUpdater.on("checking-for-update", () => {
+  sendStatusToWindow("Checking for update...");
 });
 
-autoUpdater.on('update-downloaded', () => {
-  mainWindow.webContents.send('update_downloaded');
+autoUpdater.on("update-available", (info) => {
+  sendStatusToWindow("Update available.");
+});
+autoUpdater.on("update-not-available", (info) => {
+  sendStatusToWindow("Update not available.");
 });
 
-ipcMain.on('restart_app', () => {
+autoUpdater.on("error", (err) => {
+  sendStatusToWindow(`Error in auto-updater: ${err.toString()}`);
+});
+
+autoUpdater.on("download-progress", (progressObj) => {
+  sendStatusToWindow(
+    `Download speed: ${progressObj.bytesPerSecond} - Downloaded ${progressObj.percent}% (${progressObj.transferred} + '/' + ${progressObj.total} + )`
+  );
+});
+
+autoUpdater.on("update-downloaded", (info) => {
+  sendStatusToWindow("Update downloaded; will install now");
+});
+
+autoUpdater.on("update-downloaded", (info) => {
+  // Wait 5 seconds, then quit and install
+  // In your application, you don't need to wait 500 ms.
+  // You could call autoUpdater.quitAndInstall(); immediately
   autoUpdater.quitAndInstall();
 });
